f41db77b68fb8478a5c1081008ed721bd6ede3da
huanglexian <527108411@qq.com>
2023-06-22 16:12:03 +0800

    9-1

---
+++ itstack-demo-netty-1-05/src/test/java/org/hlx/demo/netty/client/test/ApiTest.java
@@ -0,0 +1,14 @@
+package org.hlx.demo.netty.client.test;
+
+/**
+ * 
+ * 
+ * 
+ */
+public class ApiTest {
+
+    public static void main(String[] args) {
+        System.out.println("hi! ");
+    }
+
+}
