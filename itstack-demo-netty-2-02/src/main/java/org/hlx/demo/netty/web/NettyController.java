af0fe38eaa9dca6565dc25a2706d157d5a5964fe
huanglexian <527108411@qq.com>
2023-06-24 00:29:10 +0800

    2-2-1

---
+++ itstack-demo-netty-2-02/src/main/java/org/hlx/demo/netty/web/NettyController.java
@@ -0,0 +1,32 @@
+package org.hlx.demo.netty.web;
+
+import org.hlx.demo.netty.server.NettyServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 
+ * 
+ * 
+ */
+@RestController
+@RequestMapping(value = "/nettyserver", method = RequestMethod.GET)
+public class NettyController {
+
+    @Resource
+    private NettyServer nettyServer;
+
+    @RequestMapping("/localAddress")
+    public String localAddress() {
+        return "nettyServer localAddress " + nettyServer.getChannel().localAddress();
+    }
+
+    @RequestMapping("/isOpen")
+    public String isOpen() {
+        return "nettyServer isOpen " + nettyServer.getChannel().isOpen();
+    }
+
+}
