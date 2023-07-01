d25f8c58d2a76df807ca968a368f59ae0a82e7c6
huanglexian <527108411@qq.com>
2023-07-01 12:52:20 +0800

    2-9-1

---
+++ itstack-demo-netty-2-09/src/main/java/org/hlx/demo/netty/client/MyChannelFutureListener.java
@@ -0,0 +1,35 @@
+package org.hlx.demo.netty.client;
+
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.EventLoop;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 
+ * 
+ * 
+ */
+public class MyChannelFutureListener implements ChannelFutureListener {
+    @Override
+    public void operationComplete(ChannelFuture channelFuture) throws Exception {
+        if (channelFuture.isSuccess()) {
+            System.out.println("itstack-demo-netty client start done. ");
+            return;
+        }
+        final EventLoop loop = channelFuture.channel().eventLoop();
+        loop.schedule(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    new NettyClient().connect("127.0.0.1", 7397);
+                    System.out.println("itstack-demo-netty client start done. ");
+                    Thread.sleep(500);
+                } catch (Exception e){
+                    System.out.println("itstack-demo-netty client start error go reconnect ... ");
+                }
+            }
+        }, 1L, TimeUnit.SECONDS);
+    }
+}
