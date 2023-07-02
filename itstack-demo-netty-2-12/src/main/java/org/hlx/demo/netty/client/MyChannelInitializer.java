2b6ed18ffdfec97fa9b68937764527b7199cd8f9
huanglexian <527108411@qq.com>
2023-07-02 20:22:50 +0800

    2-12-2

--- itstack-demo-netty-2-12/src/main/java/org/hlx/demo/netty/client/MyChannelInitializer.java
+++ itstack-demo-netty-2-12/src/main/java/org/hlx/demo/netty/client/MyChannelInitializer.java
@@ -1,12 +1,11 @@
-package org.hlx.demo.netty.server;
+package org.hlx.demo.netty.client;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
-import io.netty.handler.stream.ChunkedWriteHandler;
-import io.netty.handler.traffic.GlobalTrafficShapingHandler;
+import io.netty.handler.traffic.ChannelTrafficShapingHandler;
 
 import java.nio.charset.Charset;
 
@@ -18,19 +17,17 @@ import java.nio.charset.Charset;
 public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
-    protected void initChannel(SocketChannel channel) {
-
+    protected void initChannel(SocketChannel channel) throws Exception {
+        //流量整形
+        channel.pipeline().addLast(new ChannelTrafficShapingHandler(10, 10));
         // 基于换行符号
         channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
-        // 流量整形；writeLimit/readLimit{0 or a limit in bytes/s}
-        channel.pipeline().addLast(new GlobalTrafficShapingHandler(channel.eventLoop().parent(), 1, 1));
         // 解码转String，注意调整自己的编码格式GBK、UTF-8
         channel.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
         // 解码转String，注意调整自己的编码格式GBK、UTF-8
         channel.pipeline().addLast(new StringEncoder(Charset.forName("GBK")));
         // 在管道中添加我们自己的接收数据实现方法
-        channel.pipeline().addLast(new MyServerHandler());
-
+        channel.pipeline().addLast(new MyClientHandler());
     }
 
 }
